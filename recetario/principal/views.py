9feddc6734f0fbe053f8b25f443f594e6040265f
cristian_ubuntu_vbox <cristian@cristian-vbox.(none)>
2012-07-08 21:25:26 -0300

    primer commit

---
+++ recetario/principal/views.py
@@ -0,0 +1,8 @@
+from principal.models import Bebida
+from django.shortcuts import render_to_response
+
+#create your views here.
+
+def lista_bebidas(request):
+	bebidas = Bebida.objects.all()
+	return render_to_response('lista_bebidas.html',{'lista':bebidas})
