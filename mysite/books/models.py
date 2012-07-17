83aedd369cfac385707cc6d2e29795f6a642a30c
cristian <cristian@cristian-PBL2021.(none)>
2012-07-17 02:22:31 -0300

    agregamos los strings de representacion ( __str__) a los modelos

--- mysite/books/models.py
+++ mysite/books/models.py
@@ -11,6 +11,8 @@ class Publisher(models.Model):
 	state_province 	= models.CharField(max_length=30)
 	country 		= models.CharField(max_length=50)
 	website 		= models.URLField()
+	def __str__(self):
+		return self.name
 
 class Author(models.Model):
     salutation 		= models.CharField(max_length=10)
@@ -18,9 +20,13 @@ class Author(models.Model):
     last_name 		= models.CharField(max_length=40)
     email 			= models.EmailField()
     headshot 		= models.ImageField(upload_to='/tmp')
+    def __str__(self):
+    	return '%s%s'% (self.first_name, self.last_name)
 
 class Book(models.Model):
 	title 			= models.CharField(max_length=100)
 	authors 		= models.ManyToManyField(Author)
 	publisher 		= models.ForeignKey(Publisher)
 	publication_date= models.DateField()
+	def __str__(self):
+		return self.title
