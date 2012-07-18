34a1921d1c9d976c125917b3d4c8d98df3fc9e4d
cristian <cristian@cristian-PBL2021.(none)>
2012-07-18 20:00:08 -0300

    prueba tripa

--- recetario/principal/models.py
+++ recetario/principal/models.py
@@ -7,25 +7,19 @@ from django.contrib.auth.models import User	#Llama al modelo de usuario
 # Create your models here.
 
 class Bebida(models.Model):
-	nombre		= models.CharField(max_length=50)
+	nombre			= models.CharField(max_length=50)
 	ingredientes	= models.TextField()
-	preparacion	= models.TextField()
+	preparacion		= models.TextField()
 	def __unicode__(self):
 		return self.nombre
 
 class Receta(models.Model):
-			# dato cadena, longtud maxima 100 y unico
-	titulo		= models.CharField(max_length=100,unique=True)
-			# dato texto, con texto de ayuda
-	ingredientes	= models.TextField(help_text='Redacta los ingredientes')
-			# dato texto, con nombre: Preparacion
-	preparacion	= models.TextField(verbose_name='Preparacion')
-			# dato imagen, se almacenaran en la carpeta recetas, titulo: Imagen
-	imagen 		= models.ImageField(upload_to='recetas',verbose_name='Imagen')
-			# dato Fecha y Hora, almacena la fecha actual
+	titulo			= models.CharField(max_length=100,unique=True)
+	ingredientes 	= models.TextField(help_text='Redacta los ingredientes')
+	preparacion		= models.TextField(verbose_name='Preparacion')
+	imagen 			= models.ImageField(upload_to='recetas',verbose_name='Imagen')
 	tiempo_registro = models.DateTimeField(auto_now=True)
-			# Enlace al modelo Usuario que Django ya tiene construido
-	usuario 	= models.ForeignKey(User)
+	usuario 		= models.ForeignKey(User)
 	def __unicode__(self):
 		return self.titulo
 
